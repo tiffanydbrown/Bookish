c1d70f727e62484ae97be042a3a1740d51025a96
Tiffany Brown <tdbrown_2001@yahoo.com>
2024-01-23 00:47:57 +0000

    Create GETs in server to create endpoints.

--- server/server.ts
+++ server/server.ts
@@ -30,10 +30,70 @@ app.use(express.static(reactStaticDir));
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+app.get('/api/bookReview', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+        from "bookReview"
+    `;
+    const result = await db.query(sql);
+    const bookReview = result.rows;
+    res.json(bookReview);
+  } catch (err) {
+    next(err);
+  }
 });
 
+app.get('/api/bookReview/:bookReviewId', async (req, res, next) => {
+  try {
+    const reviewId = Number(req.params.bookReviewId);
+    if (!Number.isInteger(reviewId) || reviewId <= 0) {
+      throw new ClientError(400, `"reviewId" must be a positive integer`);
+    }
+    const sql = `
+      select *
+        from "bookReview"
+        where "bookReviewId" = $1
+    `;
+    const params = [reviewId];
+    const result = await db.query(sql, params);
+    const review = result.rows[0];
+    if (!review) {
+      throw new ClientError(
+        404,
+        `Cannot find review with "reviewId" ${reviewId}`
+      );
+    }
+    res.json(review);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/api/bookReview', async (req, res, next) => {
+  try {
+    const { name, course, score } = req.body;
+    if (!name) throw new ClientError(400, 'User did not input name');
+    if (!course) throw new ClientError(400, 'User did not input course');
+    if (!score) throw new ClientError(400, 'User did not input score');
+    if (!Number.isInteger(score) || +score < 0 || score > 100) {
+      throw new ClientError(400, 'Score is not an integer between 0 and 100');
+    }
+
+    const sql = `
+      insert into "grades" ("course", "name", "score")
+        values($1, $2, $3)
+      returning *;
+    `;
+
+    const params = [course, name, +score];
+    const result = await db.query(sql, params);
+    const grade = result.rows[0];
+    res.json(grade);
+  } catch (err) {
+    next(err);
+  }
+});
 /*
  * Middleware that handles paths that aren't handled by static middleware
  * or API route handlers.
