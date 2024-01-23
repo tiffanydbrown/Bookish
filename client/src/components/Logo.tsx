ee54f4c50ffe72eb0243fb11cc739546fd44ab21
Tiffany Brown <tdbrown_2001@yahoo.com>
2024-01-23 23:27:07 +0000

    Changed logo from assets to image folder.

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <div className="flex flex-wrap justify-center">
-      <img src="/src/assets/Bookish.png" alt="logo" />
+      <img src="/images/Bookish.png" alt="logo" />
     </div>
   );
 }
